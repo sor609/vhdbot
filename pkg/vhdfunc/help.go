5bc28906d57a16d69d887a66295c34cc3df08561
Sasha <sstiasni@hotmail.com>
2021-05-08 20:42:34 +0800

    initial

---
+++ pkg/vhdfunc/help.go
@@ -0,0 +1,16 @@
+package vhdfunc
+
+// Self-explanatory, print help if user asks bot
+// for command list
+func printHelp() string {
+	help := "```ini\n"
+	help += "[Bot Commands]\n"
+	help += `
+	start  - start Valheim server
+	stop   - stop Valheim server
+	status - get Valheim server status
+	rebot  - reboot Bot`
+	help += "```"
+
+	return help
+}
